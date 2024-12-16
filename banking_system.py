ae3f8715e431019787e714a91da96151aabfa369
piadozo-edriane <doidoiwey@gmail.com>
2024-12-16 15:48:09 +0800

    Update file 90%

--- banking_system.py
+++ banking_system.py
@@ -44,6 +44,7 @@ class BankAccount:
             if account.name == user_name:
                 print ("Account Exist")
                 return
+            
         print ("Your account did'nt exist")
 
     def balance ():
@@ -56,13 +57,31 @@ class BankAccount:
                     deposit = int(input("Enter the amount you want to deposist: "))
                     account.balance_bank += deposit
                     print (f"your current balance is: {account.balance_bank}")
-                if account.balance_bank == 0:
+                elif account.balance_bank <= 0:
                     print ("You can't withdraw")
                     print ("Your balance is zero")
-                    if choice == "W":
-                        withdraw_ammount = int(input("Enter the amount you want to withdraw: "))
-                        account.balance_bank -= withdraw_ammount
-                        print (f"your current balance is: {account.balance_bank}")
+                    return
+                elif choice == "W":
+                    withdraw_ammount = int(input("Enter the amount you want to withdraw: "))
+                    account.balance_bank -= withdraw_ammount
+                    print (f"your current balance is: {account.balance_bank}")
+            elif account.pin != user_pin:
+                print ("User pin is incorrect")
+                return
+        
+    def transfer_balance ():
+        for account in list_account:
+            check_user = str(input("Enter your name: "))
+            if account.name == check_user:
+                transfer_balance = account.balance_bank
+                transfer_user = str(input("Enter the name you want to transfer: "))
+                transfer_balance = int(input("Enter the amount you want to transfer: "))
+                account.balance_bank -= transfer_balance
+                print (f"Your total balance is: {account.balance_bank}")
+
+            if account.name == transfer_user:
+                account.balance_bank += transfer_balance
+                print (f"Your total balance is: {account.balance_bank}")
 
 while True:
     os.system("cls")
@@ -70,23 +89,31 @@ while True:
     print ("2. Display account")
     print ("3. Account checker")
     print ("4. Deposit and Withdraw checker")
-    print ("5. Exit")    
+    print ("5. Transfer to another user")
+    print ("6. Exit")    
     choice = input("Enter your choice: ")
 
-    if choice == "5":
+    if choice == "6":
         break
     match choice: 
-        case "1": 
+        case "1":
+            os.system("cls")
             BankAccount.input_account()
             input()
-        case "2": 
+        case "2":
+            os.system("cls") 
             BankAccount.display_account()
             input()
         case "3":
+            os.system("cls")
             BankAccount.account_checker()
             input()
         case "4":
+            os.system("cls")
             BankAccount.balance()
+            input()
+        case "5":
+            BankAccount.transfer_balance()
             input()
         case _:
             print ("Invalid choice")
